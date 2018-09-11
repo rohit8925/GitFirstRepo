be50980c2632c11252cce449233e75654197574b
rohit8925 <rohit.tiwari0025@gmail.com>
2018-09-11 20:02:26 +0530

    Update seed.groovy

--- seed.groovy
+++ seed.groovy
@@ -1,1 +1,23 @@
+import groovy.json.JsonSlurper
 
+def project = 'DSL-Exemple'
+
+
+    def jsonSlurper = new JsonSlurper()
+    def reader = new BufferedReader(new InputStreamReader(new FileInputStream("seed.groovy"),"UTF-8"));
+    data = jsonSlurper.parse(reader); 
+
+
+
+data.TST.each {
+    def branchName = it.repositoryName
+    def jobName = "${project}-${branchName}".replaceAll('/','-')
+    job(jobName) {
+        scm {
+            git("git://github.com/${project}.git", branchName)
+        }
+        steps {
+            maven("test -Dproject.name=${project}/${branchName}")
+        }
+    }
+}
